1113da994d6e90a19a03b7120731c15ceade2b1a
Sdqumar <Sdqumar09@gmail.com>
2021-06-11 23:48:59 +0100

    ..

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
@@ -25,31 +24,3 @@ try {
  export default firebase
 
 
-=======
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/storage'
-
-const config = {
-  apiKey: "AIzaSyBuX9Tg0LK7EnFZGkoXDPwm6mDeKOJyVJE",
-  authDomain: "sdqumar007.firebaseapp.com",
-  projectId: "sdqumar007",
-  storageBucket: "sdqumar007.appspot.com",
-  messagingSenderId: "619808274792",
-  appId: "1:619808274792:web:6daec642a20eae962abedd",
-  measurementId: "G-EBM1PZ4W6K"
-};
-
-try {
-  firebase.initializeApp(config);
-} catch(err){
-  if (!/already exists/.test(err.message)) {
-    console.error('Firebase initialization error', err.stack)}
-}
-
-
- export default firebase
-
-
->>>>>>> master
--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import admin from 'firebase-admin';
 import serviceAccount from './serviceAccountKey.json';
 
@@ -12,19 +11,4 @@ if (!admin.apps.length) {
     console.log('Firebase admin initialization error', error.stack);
   }
 }
-=======
-import admin from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json';
-
-if (!admin.apps.length) {
-  try {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: "YOUR_DB_URL"
-    });
-  } catch (error) {
-    console.log('Firebase admin initialization error', error.stack);
-  }
-}
->>>>>>> master
 export default admin.firestore();
