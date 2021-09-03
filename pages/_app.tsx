06f87a4fa487b48749281a42295dfc572a9f40f9
Sdqumar <Sdqumar09@gmail.com>
2021-09-03 10:25:23 +0100

    testing mobile blank page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,32 +33,30 @@ function MyApp({ Component, pageProps }) {
 	}, [currentUser]);
 
 	return (
-		!loading && (
-			<ChakraProvider theme={theme}>
-				<AuthContext.Provider value={currentUser}>
-					<Grid
-						templateRows="6rem auto 4rem"
-						templateColumns="auto"
-						height="100vh"
-						maxW="100%"
+		<ChakraProvider theme={theme}>
+			<AuthContext.Provider value={currentUser}>
+				<Grid
+					templateRows="6rem auto 4rem"
+					templateColumns="auto"
+					height="100vh"
+					maxW="100%"
+					overflow-x="hidden"
+				>
+					<Header />
+
+					<Flex
+						w="100%"
+						m={{ base: 'auto', md: '1px' }}
+						mt={{ base: '0', md: '0rem' }}
+						position="relative"
 						overflow-x="hidden"
 					>
-						<Header />
-
-						<Flex
-							w="100%"
-							m={{ base: 'auto', md: '1px' }}
-							mt={{ base: '0', md: '0rem' }}
-							position="relative"
-							overflow-x="hidden"
-						>
-							<Component {...pageProps} />
-						</Flex>
-						<Footer />
-					</Grid>
-				</AuthContext.Provider>
-			</ChakraProvider>
-		)
+						<Component {...pageProps} />
+					</Flex>
+					<Footer />
+				</Grid>
+			</AuthContext.Provider>
+		</ChakraProvider>
 	);
 }
 
