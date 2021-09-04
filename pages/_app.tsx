d6f55e96c871b0ecd8a26d96d84c61cd468b1b5d
Sdqumar <Sdqumar09@gmail.com>
2021-09-04 11:44:21 +0100

    Back charka ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,24 @@
-import Grid from '@material-ui/core/Grid';
+import { Flex, Grid, ChakraProvider } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import firebase from '../config/firebase-config';
-//import Header from '../components/Header';
+import Header from '../components/Header';
 import AuthContext from '../components/AuthContext';
-//import Footer from '../components/Footer';
-//import '../styles/globals.css';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Button from '@material-ui/core/Button';
+import Footer from '../components/Footer';
+import '../styles/globals.css';
+
+import { extendTheme } from '@chakra-ui/react';
+
+const Link = {
+	baseStyle: {
+		textDecoration: 'none',
+	},
+};
+
+const colors = {
+	primary: '#fbae17',
+};
+const theme = extendTheme({ colors, Link });
 
 function MyApp({ Component, pageProps }) {
 	const [currentUser, setCurrentUser] = useState<undefined | {}>(undefined);
@@ -24,12 +35,8 @@ function MyApp({ Component, pageProps }) {
 	return (
 		!loading && (
 			<AuthContext.Provider value={currentUser}>
-				<CssBaseline />
-				<Button variant="contained">Default</Button>
-				<Button variant="contained" color="primary">
-					Primary
-				</Button>
-				{/* <Grid
+				<ChakraProvider theme={theme}>
+					<Grid
 						templateRows="6rem auto 4rem"
 						templateColumns="auto"
 						height="100vh"
@@ -48,7 +55,8 @@ function MyApp({ Component, pageProps }) {
 							<Component {...pageProps} />
 						</Flex>
 						<Footer />
-					</Grid> */}
+					</Grid>
+				</ChakraProvider>
 			</AuthContext.Provider>
 		)
 	);
