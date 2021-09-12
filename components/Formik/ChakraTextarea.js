6b0c7ff81384893787b7b1daddd42b0d4f748656
Sdqumar <Sdqumar09@gmail.com>
2021-09-12 03:21:35 +0100

    fix matrial uo build err

--- components/Formik/ChakraTextarea.js
+++ components/Formik/ChakraTextarea.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { Field } from 'formik';
-import {
-	FormControl,
-	FormLabel,
-	FormErrorMessage,
-	Textarea,
-} from '@chakra-ui/react';
+// import React from 'react';
+// import { Field } from 'formik';
+// import {
+// 	FormControl,
+// 	FormLabel,
+// 	FormErrorMessage,
+// 	Textarea,
+// } from '@chakra-ui/react';
 
-function ChakraTextarea(props) {
-	const { label, name, ...rest } = props;
-	return (
-		<Field name={name}>
-			{({ field, form }) => (
-				<FormControl
-					isInvalid={form.errors[name] && form.touched[name]}
-					isRequired
-				>
-					<FormLabel htmlFor={name}>{label}</FormLabel>
-					<Textarea id={name} {...rest} {...field} minH="28vh" />
-					<FormErrorMessage>{form.errors[name]}</FormErrorMessage>
-				</FormControl>
-			)}
-		</Field>
-	);
-}
-export default ChakraTextarea;
+// function ChakraTextarea(props) {
+// 	const { label, name, ...rest } = props;
+// 	return (
+// 		<Field name={name}>
+// 			{({ field, form }) => (
+// 				<FormControl
+// 					isInvalid={form.errors[name] && form.touched[name]}
+// 					isRequired
+// 				>
+// 					<FormLabel htmlFor={name}>{label}</FormLabel>
+// 					<Textarea id={name} {...rest} {...field} minH="28vh" />
+// 					<FormErrorMessage>{form.errors[name]}</FormErrorMessage>
+// 				</FormControl>
+// 			)}
+// 		</Field>
+// 	);
+// }
+// export default ChakraTextarea;
