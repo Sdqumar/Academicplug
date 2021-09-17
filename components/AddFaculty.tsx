086833eda7b36e9feb0f303dc556a5c7f45e66f7
Sdqumar <sdqumar09@gmail.com>
2021-09-17 12:13:48 +0000

    fix error

--- components/AddFaculty.tsx
+++ components/AddFaculty.tsx
@@ -1,44 +1,40 @@
-import { Formik, Form } from 'formik';
+import {Formik, Form} from 'formik';
 import * as Yup from 'yup';
 import FormikControl from './Formik/FormikControl';
 import firebase from '../config/firebase-config';
-import { Box, Button, Typography } from '@material-ui/core';
-import toast, { Toaster } from 'react-hot-toast';
+import {Box, Button, Typography} from '@material-ui/core';
+import toast, {Toaster} from 'react-hot-toast';
 import Dialog from '@material-ui/core/Dialog';
 import React, {useState} from 'react';
 
-function AddFaculty({ school }) {
-	const initialValues = {
-		faculty: '',
-	};
+function AddFaculty({school}) {
+    const initialValues = {
+        faculty: ''
+    };
 
-	const validationSchema = Yup.object().shape({
-		faculty: Yup.mixed().required('Required'),
-	});
+    const validationSchema = Yup.object().shape({faculty: Yup.mixed().required('Required')});
 
-	const onSubmit = async (values, actions) => {
-		actions.setSubmitting(true);
+    const onSubmit = async (values, actions) => {
+        actions.setSubmitting(true);
 
-		const { doc, setDoc, getFirestore } = await import('firebase/firestore');
-		const firestore = getFirestore(firebase);
+        const {doc, setDoc, getFirestore} = await import ('firebase/firestore');
+        const firestore = getFirestore(firebase);
 
-		const faculty = values.faculty.trim();
+        const faculty = values.faculty.trim();
 
-		setDoc(doc(firestore, 'schools', school, 'faculty', faculty), {
-			name: faculty,
-			department: [],
-		})
-			.then(() => {
-				toast.success('Faculty Added!');
+        setDoc(doc(firestore, 'schools', school, 'faculty', faculty), {
+            name: faculty,
+            department: []
+        }).then(() => {
+            toast.success('Faculty Added!');
 
-				actions.resetForm();
-				actions.setSubmitting(false);
-			})
-			.catch((error) => {
-				console.error('Error writing document: ', error);
-			});
-	};
-	const [open, setOpen] = useState(false);
+            actions.resetForm();
+            actions.setSubmitting(false);
+        }).catch((error) => {
+            console.error('Error writing document: ', error);
+        });
+    };
+    const [open, setOpen] = useState(false);
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -47,18 +43,17 @@ function AddFaculty({ school }) {
     const handleClose = () => {
         setOpen(false);
     };
-	return (
-		<>
+    return (
+        <>
             <Button variant="outlined"
                 onClick={handleClickOpen}>
                 Add Faculty
             </Button>
             <Dialog open={open}
                 onClose={handleClose}
-				maxWidth={'lg'}
-				fullWidth={true}
-				className='AddDialog'
-				>
+                maxWidth={'lg'}
+                fullWidth={true}
+                className='AddDialog'>
                 <Box justifyContent="space-between" mt="1rem"
                     ml={
                         {
@@ -72,50 +67,38 @@ function AddFaculty({ school }) {
                         Close
                     </button>
                 </Box>
-				<Box
-			alignItems="center"
-			justifyContent="center"
-			mb="2rem"
-			m="auto"
-			display="flex"
-		>
-			<Formik
-				initialValues={initialValues}
-				validationSchema={validationSchema}
-				onSubmit={onSubmit}
-			>
-				{(formik) => {
-					return (
-						<Form>
-							<Box mt="20px">
-								<FormikControl
-									control="chakraInput"
-									type="name"
-									label="Faculty Name"
-									name="faculty"
-								/>
-							</Box>
-							<Toaster position="top-center" />
+                <Box alignItems="center" justifyContent="center" mb="2rem" m="auto" display="flex">
+                    <Formik initialValues={initialValues}
+                        validationSchema={validationSchema}
+                        onSubmit={onSubmit}>
+                        {
+                        (formik) => {
+                            return (
+                                <Form>
+                                    <Box mt="20px">
+                                        <FormikControl control="chakraInput" type="name" label="Faculty Name" name="faculty"/>
+                                    </Box>
+                                    <Toaster position="top-center"/>
 
-							<Box mt={2} textAlign="center">
-								<Button
-									variant="outlined"
-									type="submit"
-									disabled={!formik.isValid}
-								>
-									Submit
-								</Button>
-							</Box>
-						</Form>
-					);
-				}}
-			</Formik>
-		</Box>
-              
+                                    <Box mt={2}
+                                        textAlign="center">
+                                        <Button variant="outlined" type="submit"
+                                            disabled={
+                                                !formik.isValid
+                                        }>
+                                            Submit
+                                        </Button>
+                                    </Box>
+                                </Form>
+                            );
+                        }
+                    } </Formik>
+                </Box>
+
             </Dialog>
         </>
-	
-	);
+
+    );
 }
 
 export default AddFaculty;
--- pages/Signup.tsx
+++ pages/Signup.tsx
@@ -1,169 +1,131 @@
 import React from 'react';
-import { Formik, Form } from 'formik';
+import {Formik, Form} from 'formik';
 import * as Yup from 'yup';
 import firebase from 'config/firebase-config';
 
-import { useRouter } from 'next/router';
-import { Box, Button, Typography } from '@material-ui/core';
+import {useRouter} from 'next/router';
+import {Box, Button, Typography} from '@material-ui/core';
 import Cookies from 'js-cookie';
-import { useContext } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
+import {useContext} from 'react';
+import toast, {Toaster} from 'react-hot-toast';
 
 import AuthContext from 'components/AuthContext';
-import FormikControl '@components/Formik/FormikControl'
+import FormikControl from '@/components/Formik/FormikControl';
 
 
 function RegistrationForm() {
-	const initialValues = {
-		email: '',
-		password: '',
-		confirmPassword: '',
-		username: '',
-	};
+    const initialValues = {
+        email: '',
+        password: '',
+        confirmPassword: '',
+        username: ''
+    };
 
-	const lowercaseRegex = /(?=.*[a-z])/;
-	const uppercaseRegex = /(?=.*[A-Z])/;
-	const numericRegex = /(?=.*[0-9])/;
+    const lowercaseRegex = /(?=.*[a-z])/;
+    const uppercaseRegex = /(?=.*[A-Z])/;
+    const numericRegex = /(?=.*[0-9])/;
 
-	const validationSchema = Yup.object({
-		username: Yup.string().required('Required'),
-		email: Yup.string().email('Invalid email format').required('Required'),
+    const validationSchema = Yup.object({
+        username: Yup.string().required('Required'),
+        email: Yup.string().email('Invalid email format').required('Required'),
 
-		password: Yup.string()
-			.required('Required')
-			.matches(lowercaseRegex, 'One lowercase required!')
-			.matches(numericRegex, 'One number required!')
-			.min(4, 'Minimum 4 character required!'),
-		confirmPassword: Yup.string()
-			.oneOf([Yup.ref('password'), ''], 'Passwords must match')
-			.required('Required'),
-	});
+        password: Yup.string().required('Required').matches(lowercaseRegex, 'One lowercase required!').matches(numericRegex, 'One number required!').min(4, 'Minimum 4 character required!'),
+        confirmPassword: Yup.string().oneOf(
+            [
+                Yup.ref('password'), ''
+            ],
+            'Passwords must match'
+        ).required('Required')
+    });
 
-	const router = useRouter();
+    const router = useRouter();
 
-	type values = {
-		email: string;
-		password: string;
-		username: string;
-	};
-	const [currentUser, setCurrentUser] = useContext(AuthContext);
+    type values = {
+        email: string;
+        password: string;
+        username: string;
+    };
+    const [currentUser, setCurrentUser] = useContext(AuthContext);
 
-	const onSubmit = async (values: values, actions) => {
-		actions.setSubmitting(true);
+    const onSubmit = async (values : values, actions) => {
+        actions.setSubmitting(true);
 
-		const displayName = values.username.toString();
-		const email = values.email.toString();
+        const displayName = values.username.toString();
+        const email = values.email.toString();
 
-		const { doc, setDoc, getDocs, getFirestore, collection, query, where } =
-			await import('firebase/firestore');
-		const firestore = getFirestore(firebase);
+        const {
+            doc,
+            setDoc,
+            getDocs,
+            getFirestore,
+            collection,
+            query,
+            where
+        } = await import ('firebase/firestore');
+        const firestore = getFirestore(firebase);
 
-		const q = query(
-			collection(firestore, 'users'),
-			where('displayName', '==', displayName)
-		);
-		const querySnapshot = await getDocs(q);
+        const q = query(collection(firestore, 'users'), where('displayName', '==', displayName));
+        const querySnapshot = await getDocs(q);
 
-		if (querySnapshot.docs.length !== 0) {
-			toast.error('Username has Already been taken!');
+        if (querySnapshot.docs.length !== 0) {
+            toast.error('Username has Already been taken!');
 
-			actions.setSubmitting(false);
-		} else {
-			const { getAuth, createUserWithEmailAndPassword, updateProfile } =
-				await import('firebase/auth');
-			const auth = getAuth(firebase);
+            actions.setSubmitting(false);
+        } else {
+            const {getAuth, createUserWithEmailAndPassword, updateProfile} = await import ('firebase/auth');
+            const auth = getAuth(firebase);
 
-			createUserWithEmailAndPassword(auth, values.email, values.password)
-				.then(({ user }) => {
-					updateProfile(auth.currentUser, {
-						displayName,
-					});
+            createUserWithEmailAndPassword(auth, values.email, values.password).then(({user}) => {
+                updateProfile(auth.currentUser, {displayName});
 
-					const uid = user.uid;
+                const uid = user.uid;
 
-					setDoc(doc(firestore, 'users', uid), {
-						email,
-						displayName,
-					});
-					setCurrentUser({ displayName, uid });
-					Cookies.set('user', JSON.stringify({ displayName, uid }));
-				})
-				.then(() => {
-					toast.success('SignUp Sucessfully!');
+                setDoc(doc(firestore, 'users', uid), {email, displayName});
+                setCurrentUser({displayName, uid});
+                Cookies.set('user', JSON.stringify({displayName, uid}));
+            }).then(() => {
+                toast.success('SignUp Sucessfully!');
 
-					actions.setSubmitting(false);
-				})
+                actions.setSubmitting(false);
+            }).catch((error) => {
+                toast.error('Email has Already been taken!');
 
-				.catch((error) => {
-					toast.error('Email has Already been taken!');
+                actions.setSubmitting(false);
+            });
+        }
+    };
 
-					actions.setSubmitting(false);
-				});
-		}
-	};
+    return (
+        <Formik initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}>
+            {
+            (formik) => {
+                return (
+                    <Box alignItems="center" justifyContent="center" maxWidth="15rem" height="content-fit" m="auto" mt="1rem" display="flex" flexWrap="wrap">
+                        <Typography className="heading">SIGN UP</Typography>
+                        <Form>
+                            <FormikControl control="chakraInput" type="username" label="Username" name="username"/>
+                            <FormikControl control="chakraInput" type="email" label="Email" name="email"/>
+                            <FormikControl control="chakraInput" type="password" label="Password" name="password"/>
+                            <FormikControl control="chakraInput" type="password" label="Confirm Password" name="confirmPassword"/>
+                            <Toaster position="top-center"/>
 
-	return (
-		<Formik
-			initialValues={initialValues}
-			validationSchema={validationSchema}
-			onSubmit={onSubmit}
-		>
-			{(formik) => {
-				return (
-					<Box
-						alignItems="center"
-						justifyContent="center"
-						maxWidth="15rem"
-						height="content-fit"
-						m="auto"
-						mt="1rem"
-						display="flex"
-						flexWrap="wrap"
-					>
-						<Typography className="heading">SIGN UP</Typography>
-						<Form>
-							<FormikControl
-								control="chakraInput"
-								type="username"
-								label="Username"
-								name="username"
-							/>
-							<FormikControl
-								control="chakraInput"
-								type="email"
-								label="Email"
-								name="email"
-							/>
-							<FormikControl
-								control="chakraInput"
-								type="password"
-								label="Password"
-								name="password"
-							/>
-							<FormikControl
-								control="chakraInput"
-								type="password"
-								label="Confirm Password"
-								name="confirmPassword"
-							/>
-							<Toaster position="top-center" />
-
-							<Box m="10px 0" textAlign="center">
-								{' '}
-								<Button
-									variant="outlined"
-									type="submit"
-									disabled={!formik.isValid}
-								>
-									Submit
-								</Button>
-							</Box>
-						</Form>
-					</Box>
-				);
-			}}
-		</Formik>
-	);
+                            <Box m="10px 0" textAlign="center">
+                                {' '}
+                                <Button variant="outlined" type="submit"
+                                    disabled={
+                                        !formik.isValid
+                                }>
+                                    Submit
+                                </Button>
+                            </Box>
+                        </Form>
+                    </Box>
+                );
+            }
+        } </Formik>
+    );
 }
 
 export default RegistrationForm;
