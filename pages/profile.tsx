e9b866e0d42e0c037a78b5401f7a4e87a2c02e0f
sdqumar <sdqumar09@gmail.com>
2022-02-03 12:47:04 +0100

    check if user is admin

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -6,6 +6,8 @@ import {
   getFirestore,
   collectionGroup,
   query,
+  doc,
+  getDoc,
   where,
   getDocs,
 } from "firebase/firestore";
@@ -16,16 +18,21 @@ import NextLink from "next/link";
 const COLUMNS = [
   {
     Header: "Course",
-    accessor: row =>{
-      
-      const {School, Faculty,Department,Course} = row
-      
-      return(
-        <NextLink href={`/${School}/${Faculty.replace(/\s/g, "-")}/${Department.replace(/\s/g, "-")}/${Course.replace(/\s/g, "-")}`} passHref>
-          <a style={{fontWeight:700}}>{Course}</a>
-          </NextLink>
-      )
-    }
+    accessor: (row) => {
+      const { School, Faculty, Department, Course } = row;
+
+      return (
+        <NextLink
+          href={`/${School}/${Faculty.replace(/\s/g, "-")}/${Department.replace(
+            /\s/g,
+            "-"
+          )}/${Course.replace(/\s/g, "-")}`}
+          passHref
+        >
+          <a style={{ fontWeight: 700 }}>{Course}</a>
+        </NextLink>
+      );
+    },
   },
   {
     Header: "Code",
@@ -58,19 +65,28 @@ export default function Profile({ data }) {
       <h1>Hello {currentUser?.displayName}</h1>
 
       <h3>List of Materials</h3>
-      {data && <AdminTable TableData={data.materials} COLUMNS={COLUMNS} />}
-      <br/>
-      <br/>
-      {/* {data && <BasicTable TableData={data.userMaterials} COLUMNS={COLUMNS} />} */}
+      {data?.user?.admin  && <AdminTable TableData={data.materials} COLUMNS={COLUMNS} />}
+      <br />
+      <br />
+      {data?.userMaterials.length > 0 && <BasicTable TableData={data.userMaterials} COLUMNS={COLUMNS} />}
     </Box>
   );
 }
 
 export async function getServerSideProps(ctx) {
   const { id } = ctx.query;
+if(id){
 
   const db = getFirestore(firebase);
 
+  const getUser = async () => {
+    const docRef = doc(db, "users", id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      return docSnap.data();
+    }
+  };
   const getUserMaterials = async () => {
     const ref = query(collectionGroup(db, "courses"), where("uid", "==", id));
     const querySnapshot = await getDocs(ref);
@@ -80,6 +96,7 @@ export async function getServerSideProps(ctx) {
     });
     return materials;
   };
+
   const getMaterials = async () => {
     const ref = query(collectionGroup(db, "courses"));
     const querySnapshot = await getDocs(ref);
@@ -90,17 +107,23 @@ export async function getServerSideProps(ctx) {
     return materials;
   };
 
-
-  const userMaterials  = await  getUserMaterials()
-  const materials = await getMaterials()
-
+  const user = await getUser();
+  const userMaterials = await getUserMaterials();
+  const materials = await getMaterials();
 
   return {
     props: {
       data: {
+        user,
         userMaterials,
-        materials
+        materials,
       },
     },
   };
+}else{
+  return {
+    notFound: true,
+  }
+}
+
 }
