25c94275cb8f7f77247cbd5b49166da3328531ec
sdqumar <sdqumar09@gmail.com>
2022-02-08 12:16:04 +0100

    recent courses  stying

--- components/Home/RecentCourses.tsx
+++ components/Home/RecentCourses.tsx
@@ -6,44 +6,49 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexWrap: "wrap",
 
-    "& h5": {
-      border: "2px solid",
+    "& h6": {
       textAlign: "center",
       padding: "0.5rem",
       fontWeight: 600,
+      textTransform: "capitalize",
+      fontSize: "100%",
     },
   },
 }));
 
 type courses = {
-  School:string;
-  Faculty:string;
-  Department:string;
-  Course:string;
-  Code:string;
-}
+  School: string;
+  Faculty: string;
+  Department: string;
+  Course: string;
+  Code: string;
+};
 type RecentCoursesProps = {
   list: courses[];
-  
 };
-const RecentCourses = ({ list,  }: RecentCoursesProps) => {
+const RecentCourses = ({ list }: RecentCoursesProps) => {
   const classes = useStyles();
 
   return (
+    <Box marginY={5}  maxWidth="55rem">
+    <Typography variant="h5" style={{marginLeft:'10px',fontWeight:600}}>
+      Recent Courses
+    </Typography>
     <Box
       mt="1rem"
       flexDirection="row"
       height="fit-content"
-      justifyContent={{ base: "center", md: "flex-start" }}
+      marginX='auto'
+      justifyContent='start'
       className={classes.grid}
     >
       {list.map((item) => {
-        const {School,Faculty,Department,Course,Code} =item
-         const schoolUrl = `/${School.replace(/\s/g, "-")}`;
-         const facultyUrl = `/${Faculty.replace(/\s/g, "-")}`;
-         const departmentUrl = `/${Department.replace(/\s/g, "-")}`;
-       
-         const url = schoolUrl + facultyUrl + departmentUrl;
+        const { School, Faculty, Department, Course, Code } = item;
+        const schoolUrl = `/${School.replace(/\s/g, "-")}`;
+        const facultyUrl = `/${Faculty.replace(/\s/g, "-")}`;
+        const departmentUrl = `/${Department.replace(/\s/g, "-")}`;
+
+        const url = schoolUrl + facultyUrl + departmentUrl;
         return (
           <NextLink
             key={Code}
@@ -51,22 +56,34 @@ const RecentCourses = ({ list,  }: RecentCoursesProps) => {
             passHref
           >
             <Link>
-              {" "}
               <Box
                 m="0 5px"
                 bgcolor="rgb(251 174 23)"
-                padding="1.2rem"
+                padding="1rem"
                 width="10rem"
-                height='fit-content'
+                height="4rem"
                 mb="0.8rem"
               >
-                <Typography variant="h5">{item.Course.trim()}</Typography>
+                <Box
+                  border="2px solid"
+                  width="100%"
+                  height="100%"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Typography variant="h6">
+                    {item.Course.toLowerCase().trim()}
+                  </Typography>
+                </Box>
               </Box>
             </Link>
           </NextLink>
         );
       })}
     </Box>
+    </Box>
+
   );
 };
 
